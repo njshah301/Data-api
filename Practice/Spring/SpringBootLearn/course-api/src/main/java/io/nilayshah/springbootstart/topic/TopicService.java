b30630d3726ed3f447897e869cecd3a1b1a7bfc0
Nilay Shah <MY_NAME@example.com>
2023-01-26 00:04:40 +0530

    done

--- Practice/Spring/SpringBootLearn/course-api/src/main/java/io/nilayshah/springbootstart/topic/TopicService.java
+++ Practice/Spring/SpringBootLearn/course-api/src/main/java/io/nilayshah/springbootstart/topic/TopicService.java
@@ -1,9 +1,7 @@
 package io.nilayshah.springbootstart.topic;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
